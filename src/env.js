3b8cd517bef3f0effc6227d913971295bf867c82
evans-g-crsj <eg.crsj@gmail.com>
2021-03-18 10:03:46 -0400

    change var name

--- src/env.js
+++ src/env.js
@@ -13,6 +13,6 @@ export const esHost = process.env.ES_HOST || 'http://localhost:9200';
 
 export const maxNOfGenomicFeatureSuggestions = process.env.MAX_NUMBER_OF_GF_SUGGESTIONS || 5;
 
-export const indexNameGenomicFeatureSuggestion = process.env.GF_INDEX_NAME;
+export const indexNameGenomicFeatureSuggestion = process.env.GENOMIC_SUGGESTIONS_INDEX_NAME;
 
 export const variantStatsIndex = process.env.VARIANT_STATS_INDEX;
