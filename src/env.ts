4c2fdd6038f8f3c677771bd6447775cfa1854fb8
Céline Pelletier <celinepelletier@CRHSJ106784.local>
2021-09-01 13:34:04 -0400

    SKFP-82: Use real realm in QA

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const port = process.env.PORT || 5050;
 
 export const keycloakURL = process.env.KEYCLOAK_URL || 'https://kf-keycloak-qa.kf-strides.org/auth';
-export const keycloakRealm = process.env.KEYCLOAK_REALM || 'KidsFirst-test-CPE';
+export const keycloakRealm = process.env.KEYCLOAK_REALM || 'kidsfirstdrc';
 export const keycloakClient = process.env.KEYCLOAK_CLIENT || 'kidsfirst-apis';
 
 export const esHost = process.env.ES_HOST || 'http://localhost:9200';
