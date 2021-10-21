3f87d0265ebb2760bbc54e0620a9125644f3234d
Celine Pelletier <cpelletier@ferlab.bio>
2021-10-21 16:49:44 -0400

    SKFP-12: Add middleware injectBodyHttpHeaders.ts

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,21 @@ import { Keycloak } from 'keycloak-connect';
 import { dependencies, version } from '../package.json';
 import { keycloakURL, esHost } from './env';
 import genomicFeatureSuggestions, { SUGGESTIONS_TYPES } from './endpoints/genomicFeatureSuggestions';
+import { injectBodyHttpHeaders } from './middleware/injectBodyHttpHeaders';
 
 export default (keycloak: Keycloak): Express => {
     const app = addAsync.addAsync(express());
 
     app.use(cors());
+
+    app.use(express.json({ limit: '50mb' }));
+    app.use(
+        express.urlencoded({
+            extended: true,
+            limit: '50mb',
+        }),
+    );
+    app.use(injectBodyHttpHeaders());
 
     app.use(
         keycloak.middleware({
