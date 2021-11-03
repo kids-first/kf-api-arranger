1116c21b8e7c66285d6b22ee0be4726001246c21
Celine Pelletier <cpelletier@ferlab.bio>
2021-11-03 12:11:26 -0400

    Remove console.log

--- src/endpoints/genomicFeatureSuggestions.ts
+++ src/endpoints/genomicFeatureSuggestions.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import EsInstance from '../ElasticSearchClientInstance';
 import {
-    esHost,
     indexNameGeneFeatureSuggestion,
     indexNameVariantFeatureSuggestion,
     maxNOfGenomicFeatureSuggestions,
@@ -19,9 +18,6 @@ export default async (req: Request, res: Response, type: string): Promise<void> 
     const client = await EsInstance.getInstance();
 
     const _index = type === SUGGESTIONS_TYPES.GENE ? indexNameGeneFeatureSuggestion : indexNameVariantFeatureSuggestion;
-
-    console.log(_index);
-    console.log(esHost);
 
     const { body } = await client.search({
         index: _index,
