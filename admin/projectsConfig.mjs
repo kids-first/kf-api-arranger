02ae013897057905fc33b06b0eecfd7ed3226b5c
evans-g-crsj <eg.crsj@gmail.com>
2022-08-19 10:58:35 -0400

    feature: SKFP-440 :construction: Extract configuration from code

--- admin/projectsConfig.mjs
+++ admin/projectsConfig.mjs
@@ -1,55 +1,11 @@
-const GRAPHQL_FIELD_STUDY = 'Study';
-const GRAPHQL_FIELD_PARTICIPANT = 'Participant';
+import config from './conf.json';
 
-// Reminder: by arranger standards, project id must be lowered case.
-const PROJECTS_IDS = {
-    migration_test_qa: 'migration_test_qa',
-};
-
-const commonMutations = [
-    {
-        field: "experimental_strategy",
-        graphqlField: GRAPHQL_FIELD_STUDY,
-        extendedFieldMappingInput: {
-            isArray: true,
-        },
-    },
-    {
-        field: "controlled_access",
-        graphqlField: GRAPHQL_FIELD_STUDY,
-        extendedFieldMappingInput: {
-            isArray: true,
-        },
-    },
-    {
-        field: "data_category",
-        graphqlField: GRAPHQL_FIELD_STUDY,
-        extendedFieldMappingInput: {
-            isArray: true,
-        },
-    }
-];
-
-const personalizeProject = (id, indices) => {
-    const lambda = x => ({ ...x, projectId: id });
-    return {
-        name: id,
-        indices: [...indices].map(lambda),
-        extendedMappingMutations: [...commonMutations].map(lambda),
-    };
-};
-
-export const projectsConfig = () => [
-    {
-        ...personalizeProject(PROJECTS_IDS.migration_test_qa, [
-            {
-                graphqlField: GRAPHQL_FIELD_STUDY,
-                esIndex: 'migration_test_study_centric',
-            },
-            {
-                graphqlField: GRAPHQL_FIELD_PARTICIPANT,
-                esIndex: 'migration_test_participant_centric',
-            },
-        ]),
-    },
-];
+export const projectsConfig = () =>
+    Object.entries(config).map(([key, value]) => {
+        const lambda = x => ({ ...x, projectId: key });
+        return {
+            name: key,
+            indices: [...value.indices].map(lambda),
+            extendedMappingMutations: [...value.extendedMappingMutations].map(lambda),
+        };
+    });
