d6cec964bba89e8ffee0888753f0620e4dc7843b
Celine Pelletier <cpelletier@ferlab.bio>
2024-10-25 10:40:38 -0400

    feat: SJIP-973 add cache for diffGeneExp

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import NodeCache from 'node-cache';
 
 export const STATISTICS_CACHE_ID = 'statistics';
-
 export const STATISTICS_PUBLIC_CACHE_ID = 'statistics_studies';
+export const TRANSCRIPTOMICS_DIFF_GENE_EXP_CACHE_ID = 'transcriptomics_diff_gene_exp';
 
 export const verifyCache = (cacheId: string, cache: NodeCache) => (
     _req: Request,
