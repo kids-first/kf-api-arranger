38fc3b90839ae82e5966dfa85fa806e313a5d5dd
evans-g-crsj <eg.crsj@gmail.com>
2025-05-05 16:22:06 -0400

    :wastebasket: Remove unused code

--- src/sqon/setSqon.ts
+++ src/sqon/setSqon.ts
@@ -1,10 +1,6 @@
-import { getUserSet } from '../endpoints/sets/setsFeature';
-import { SetSqon } from '../endpoints/sets/setsTypes';
 import { participantBiospecimenKey, participantFileKey, participantKey } from '../fieldsKeys';
 import { getUserSets } from '../userApi/userApiClient';
 import { Content, Sqon } from './types';
-
-const setRegex = /^set_id:(.+)$/gm;
 
 const getPathToParticipantId = (type: string) => {
     if (type === 'biospecimen') {
@@ -14,39 +10,6 @@ const getPathToParticipantId = (type: string) => {
     } else {
         return participantKey;
     }
-};
-
-const CAPTURING_GROUP_OF_SET_ID_VALUE = 1;
-/** @deprecated - Sometimes it "removes" important entries */
-export const replaceSetByIds = async (sqon: SetSqon, accessToken: string): Promise<SetSqon> => {
-    if (!sqon) {
-        throw new Error('Sqon is missing');
-    }
-    const contents = [];
-
-    for (const content of sqon.content) {
-        const handleContent = async content => {
-            const matchesSet = setRegex.exec(content?.content?.value?.[0]);
-            const setId: string | null = matchesSet && matchesSet[CAPTURING_GROUP_OF_SET_ID_VALUE];
-            if (setId) {
-                const set = await getUserSet(accessToken, setId);
-                const newContent = { ...content };
-                newContent.content.field = getPathToParticipantId(set.content.setType);
-                newContent.content.value = set.content.ids;
-                contents.push(newContent);
-            } else {
-                contents.push(content);
-            }
-        };
-        if (Array.isArray(content.content)) {
-            for (const deepContent of content.content) {
-                await handleContent(deepContent);
-            }
-        } else {
-            await handleContent(content);
-        }
-    }
-    return { op: 'and', content: contents };
 };
 
 export const resolveSetIds = async (sqon: Sqon, accessToken: string): Promise<Sqon> => {
